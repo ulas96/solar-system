fc23fb37330824b2933a11055c5c545a70d08be4
Ulas <y.ulas.yildiz@gmail.com>
2023-09-26 11:29:45 +0300

    file initations

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+
+  return (
+        <div className="container">
+          <div className="sun"></div>
+          <div className="earth">
+          </div>
+        </div>
+  )
+}
+
+export default App
+
+// <div className="moon"></div>
